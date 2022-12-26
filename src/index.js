1730c429fb7c7e87ca16eadc47cd0dfb5c857420
Nick <ntoukas.nick@gmail.com>
2022-12-26 00:32:29 -0500

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const BookList = () => {
+  return (
+    <div>
+      <Book />
+      <Book />
+      <Book />
+      <Book />
+    </div>
+  );
+};
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => (
+  <img
+    src='https://images-na.ssl-images-amazon.com/images/I/91MowzF+WZL._AC_UL600_SR600,400_.jpg'
+    alt='The Light We Carry: Overcoming in Uncertain Times'
+  />
+);
+const Title = () => <h2>The Light We Carry: Overcoming in Uncertain Times</h2>;
+const Author = () => <p>Michelle Obama</p>;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList />);
