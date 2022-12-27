9a87ea6857c92f34221b49f23e5d08750545ef25
Nick <ntoukas.nick@gmail.com>
2022-12-27 17:45:38 -0500

    dynamically render books onto page

--- src/index.js
+++ src/index.js
@@ -2,15 +2,34 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-const author = 'Michelle Obama';
-const title = 'The Light We Carry: Overcoming in Uncertain Times';
-const img = './images/book1.jpeg';
+const books = [
+  {
+    author: 'Michelle Obama',
+    title: 'The Light We Carry: Overcoming in Uncertain Times',
+    img: './images/book1.jpeg',
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title:
+      'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+    img: './images/book2.jpeg',
+    id: 2,
+  },
+  {
+    author: 'Colleen Hoover',
+    title: 'It Starts with Us: A Novel (It Ends with Us) ',
+    img: './images/book3.jpeg',
+    id: 3,
+  },
+];
 
 const BookList = () => {
   return (
     <section className='booklist'>
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} img={img} />
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
     </section>
   );
 };
@@ -18,7 +37,7 @@ const BookList = () => {
 const Book = ({ img, title, author }) => {
   return (
     <article className='book'>
-      <img src={img} alt='The Light We Carry: Overcoming in Uncertain Times' />
+      <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
     </article>
