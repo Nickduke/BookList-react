f91e59ade722403833bdb05c10fb74c9b0d6bc84
Nick <ntoukas.nick@gmail.com>
2022-12-28 19:43:55 -0500

    display index number of item

---
+++ src/books.js
@@ -0,0 +1,25 @@
+import img1 from './images/book1.jpeg';
+import img2 from './images/book2.jpeg';
+import img3 from './images/book3.jpeg';
+
+export const books = [
+  {
+    author: 'Michelle Obama',
+    title: 'The Light We Carry: Overcoming in Uncertain Times',
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title:
+      'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Colleen Hoover',
+    title: 'It Starts with Us: A Novel (It Ends with Us) ',
+    img: img3,
+    id: 3,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,46 +1,17 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
 
-const books = [
-  {
-    author: 'Michelle Obama',
-    title: 'The Light We Carry: Overcoming in Uncertain Times',
-    img: './images/book1.jpeg',
-    id: 1,
-  },
-  {
-    author: 'James Clear',
-    title:
-      'Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones',
-    img: './images/book2.jpeg',
-    id: 2,
-  },
-  {
-    author: 'Colleen Hoover',
-    title: 'It Starts with Us: A Novel (It Ends with Us) ',
-    img: './images/book3.jpeg',
-    id: 3,
-  },
-];
+import { books } from './books';
+import Book from './Book';
 
 const BookList = () => {
   return (
     <section className='booklist'>
-      {books.map((book) => {
-        return <Book {...book} key={book.id} />;
+      {books.map((book, index) => {
+        return <Book {...book} key={book.id} number={index} />;
       })}
     </section>
-  );
-};
-
-const Book = ({ img, title, author }) => {
-  return (
-    <article className='book'>
-      <img src={img} alt={title} />
-      <h2>{title}</h2>
-      <h4>{author}</h4>
-    </article>
   );
 };
 
